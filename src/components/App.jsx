54f949a412761fd6afac5730b9295fe1c0956f8e
figatix <60873339+figatix@users.noreply.github.com>
2023-03-05 12:16:07 +0200

    ++//

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { About } from '../pages/About';
+import { Products } from '../pages/Products';
+import { ProductDetails } from '../pages/ProductDetails';
+// import { NotFound } from '../pages/NotFound';
+import styled from 'styled-components';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+
+      <nav>
+        <SlyledNavLink to='/' end>Home</SlyledNavLink>
+        <SlyledNavLink to="/about">About</SlyledNavLink>
+        <SlyledNavLink to="/products">Products</SlyledNavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </div>
   );
 };
+
+
+const SlyledNavLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`
