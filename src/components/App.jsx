667399f14434d1cbc3fa0d258db2d692e1a796f9
figatix <60873339+figatix@users.noreply.github.com>
2023-03-05 12:29:32 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { About } from '../pages/About';
 import { Products } from '../pages/Products';
 import { ProductDetails } from '../pages/ProductDetails';
+import { Container, Header, Logo, StyledNavLink } from './App.styled';
 // import { NotFound } from '../pages/NotFound';
-import styled from 'styled-components';
+
 
 
 export const App = () => {
   return (
-    <div>
-
-      <nav>
-        <SlyledNavLink to='/' end>Home</SlyledNavLink>
-        <SlyledNavLink to="/about">About</SlyledNavLink>
-        <SlyledNavLink to="/products">Products</SlyledNavLink>
-      </nav>
+    <Container>
+      <Header>
+        <Logo> figatix </Logo>
+        <nav>
+          <StyledNavLink to='/' end>Home</StyledNavLink>
+          <StyledNavLink to="/about">About</StyledNavLink>
+          <StyledNavLink to="/products">Products</StyledNavLink>
+        </nav>
+      </Header>
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -24,15 +27,6 @@ export const App = () => {
         <Route path="/products/:productId" element={<ProductDetails />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
-    </div>
+    </Container>
   );
 };
-
-
-const SlyledNavLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`
